0d2c1225b3beb5c0050e93fd609d3af72378b4ef
XUKUNYUAN <xky13938334565@163.com>
2024-07-10 00:11:48 +0800

    update ci

---
+++ tests/cicv.rs
@@ -0,0 +1,12 @@
+use assert_cmd::prelude::*;
+use std::process::Command;
+
+#[test]
+fn cicvverify() {
+    Command::cargo_bin("rustlings")
+        .unwrap()
+        .args(&["cicvverify"]) 
+        // .current_dir("exercises")
+        .assert()
+        .success();
+}
